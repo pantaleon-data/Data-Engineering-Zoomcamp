774320e0226e60f4e2d8100773f23fb94b09ee79
Pantaleon Akujobi <dabereakujobi@gmail.com>
2026-05-30 07:36:46 +0000

    docker network and pipeline

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-from calendar import month
-
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -34,20 +30,28 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
-
-    prefix ='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres username')
+@click.option('--pg-pass', default='root', show_default=True, help='Postgres password')
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default=5432, show_default=True, help='Postgres port', type=int)
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database name')
+@click.option('--year', default=2021, show_default=True, help='Year of the taxi data', type=int)
+@click.option('--month', default=1, show_default=True, help='Month of the taxi data', type=int)
+@click.option('--target-table', default='yellow_taxi_data', show_default=True, help='Target Postgres table name')
+@click.option('--chunksize', default=100000, show_default=True, help='CSV read chunk size', type=int)
+def run(
+    pg_user,
+    pg_pass,
+    pg_host,
+    pg_port,
+    pg_db,
+    year,
+    month,
+    target_table,
+    chunksize,
+):
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
@@ -56,7 +60,7 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=chunksize
+        chunksize=chunksize,
     )
 
     first = True
@@ -64,13 +68,15 @@ def run():
         if first:
             df_chunk.head(0).to_sql(
                 name=target_table,
-                con=engine, if_exists='replace'
+                con=engine,
+                if_exists='replace',
             )
             first = False
 
         df_chunk.to_sql(
             name=target_table,
-            con=engine, if_exists='append'
+            con=engine,
+            if_exists='append',
         )
 
 
